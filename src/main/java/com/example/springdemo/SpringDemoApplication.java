824101eb8a062f593aae13de55ecae28b8efa690
Emilio Vazquez <emiliofv.sd@gmail.com>
2023-09-16 12:49:25 -0700

    initial commit

---
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController // describes an endpoint  
+public class SpringDemoApplication {
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+	@GetMapping("/hello") // specifies 
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) 
+	{
+		return String.format("Hello, %s!", name);
+	}
+}
